25f3d8f6dec4b13b2408567d9901bbc03a6d751d
Hyo Chang Kim <hck@hyochangkim.com>
2018-01-30 20:23:14 -0500

    Project setup

---
+++ Rain/View Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rain
+//
+//  Created by Hyo Kim on 1/30/18.
+//  Copyright © 2018 Hyo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
